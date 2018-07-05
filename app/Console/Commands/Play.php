6cea2badc31c4c46912a4fae57fbb9364527889a
Anthony Budd <anthonybudd94@gmail.com>
2018-07-05 13:09:39 +0100

    finished

--- app/Console/Commands/Play.php
+++ app/Console/Commands/Play.php
@@ -22,6 +22,20 @@ class Play extends Command
     protected $description = 'Play Higher or Lower';
 
     /**
+     * User score
+     *
+     * @var int
+     */
+    protected $score = 0;
+    
+    /**
+     * Cards
+     *
+     * @var int
+     */
+    protected $cards;
+
+    /**
      * Create a new command instance.
      *
      * @return void
@@ -32,12 +46,105 @@ class Play extends Command
     }
 
     /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function splashScreen()
+    {
+        $this->line('');
+        $this->line('');
+        $this->line('');
+
+        $splashscreen = file_get_contents(storage_path('splashscreen.txt'));
+        foreach(preg_split("/((\r?\n)|(\r\n?))/", $splashscreen) as $line){
+            $this->line($line);
+        }
+
+        $this->line('');
+        $this->line('');
+        $this->line('');
+        $this->line('');
+    }
+
+    public function displayCard(Card $card) {
+        $lines = [
+            ['┌─────────┐'],
+            ['│v        │'],
+            ['│         │'],
+            ['│         │'],
+            ['│    s    │'],
+            ['│         │'],
+            ['│         │'],
+            ['│         │'],
+            ['└─────────┘']
+        ];
+
+        foreach ($lines as $line) {
+            $line = str_replace('s', $card->getSymbol(), $line);
+            $line = str_replace('v', $card->value, $line);
+            $this->line($line);
+        }
+    }
+
+    public function gameOver()
+    {
+        $this->error('GAME OVER!!');
+    }
+
+    public function newRound()
+    {
+        if($this->score !== 0){
+            $this->line(sprintf('Your current score is %d', $this->score));
+            $this->line('');
+            $this->line('');
+
+            $this->line('#################################');
+            $this->line(sprintf('Round %d:', $this->score+1));
+            $this->line('');
+        }
+
+        $card = $this->cards->random(1)->first();
+        $this->line('Your card:');
+        $this->displayCard($card);
+
+        $nextCard = $this->cards->random(1)->first();
+
+        // @todo bad user input? 
+        $guess = $this->anticipate('Higher or Lower?', ['higher', 'lower']);
+        
+        
+        if ($guess === 'higher' && $card->isHigherThan($nextCard)) {
+            $this->score++;
+            $this->line('');
+            $this->line('');
+            $this->line('Correct!');
+            $this->displayCard($nextCard);
+            $this->newRound();
+        } elseif ($guess === 'lower' && $card->isLowerThan($nextCard)) {
+            $this->score++;
+            $this->line('');
+            $this->line('');
+            $this->line('Correct!');
+            $this->displayCard($nextCard);
+            $this->newRound();
+        } else {
+            $this->line('');
+            $this->line('');
+            $this->gameOver();
+            $this->displayCard($nextCard);
+        }
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        Card::getCards();
-    }
+        $this->splashScreen();
+        $this->cards = Card::getCards();
+        $this->newRound();
+    }    
 }
--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,10 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Curl\Curl;
+use Exception;
 
 class Card extends Model
 {
-    
+    protected $fillable = [
+        'value', 'suit',
+    ];
+
+    public function isSuit($suit)
+    {
+        return $suit === $this->suit;
+    }
+
+    public function getSymbol()
+    {
+        switch($this->suit){
+            case 'spades':
+                return '♠';
+            case 'diamonds':
+                return '♦';
+            case 'hearts':
+                return '♥';
+            case 'clubs':
+                return '♣';
+        }
+    }
+
     public static function getCards()
     {
         $curl = new Curl();
@@ -18,6 +41,36 @@ class Card extends Model
             return;
         }
         
-        dd($curl->response);
+        $response = $curl->response;
+        if (!is_array($response) || count($response) !== 52) {
+            throw new Exception('Bad API response');
+        }
+
+        $collection = collect();
+        foreach ($response as $key => $card) {
+            $collection->push(new Card([
+                'value' => $card->value,
+                'suit' => $card->suit,
+            ]));
+        }
+        $collection->shuffle();
+        return $collection;
     }
+
+
+    public function isHigherThan(Card $otherCard)
+    {
+        $table = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+        $cardValue = array_search($this->value, $table);
+        $otherCardValue = array_search($otherCard->value, $table);
+        return $cardValue > $otherCardValue;
+    }
+
+    public function isLowerThan(Card $otherCard)
+    {
+        $table = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+        $cardValue = array_search($this->value, $table);
+        $otherCardValue = array_search($otherCard->value, $table);
+        return $cardValue < $otherCardValue;
+    }   
 }
